2465647fa1c93dc1de8ff2d40152c792f0a47fd9
0xshrimpsoup <xshrimpsoup@proton.me>
2023-09-26 17:59:18 +0200

    feat: usePathFinder enhancements

--- src/Hooks/usePathFinder.ts
+++ src/Hooks/usePathFinder.ts
@@ -1,176 +1,165 @@
-import { useCallback } from "react";
-import { usePools } from "./usePools"
-import { Address } from "wagmi";
-import { useRouter } from "./useRouter";
+import { useCallback } from 'react';
+import { usePools } from './usePools';
+import { Address } from 'wagmi';
+import { useRouter } from './useRouter';
+import { useUniswap } from './useUniswap';
+import { zeroAddress } from 'viem';
 
-type PoolData = [`0x${string}`, `0x${string}`, `0x${string}`]
+// token0, token1, pairAddress
+type PoolData = [`0x${string}`, `0x${string}`, `0x${string}`];
 
 export const usePathFinder = () => {
     const { getAmountsOut, getAmountsIn } = useRouter();
-    const { getPools, pools } = usePools();
+    const { pools } = usePools();
+    const { uniswapConfig } = useUniswap();
 
-    
-    const getBestPathWithWithExactIn = useCallback(
+    const tokenOutWhenSwapPool = useCallback((pool: PoolData, tokenIn: Address): Address | undefined => {
+        if (compareAddress(pool[0], tokenIn)) {
+            return pool[1];
+        } else if (compareAddress(pool[1], tokenIn)) {
+            return pool[0];
+        } else {
+            return undefined;
+        }
+    }, []);
+
+    const getAllPaths = useCallback(
+        (tokenSource: Address, tokenDestination: Address, pools: PoolData[]) => {
+            const paths: Address[][] = [];
+            const normalizedTokenDestination = tokenDestination === zeroAddress ? uniswapConfig.weth : tokenDestination;
+            const normalizedTokenSource = tokenSource === zeroAddress ? uniswapConfig.weth : tokenSource;
+            const MAX_DEPTH = 5;
+
+            const computeRoute = (currentRoute: Address[], depth: number, availablePools: PoolData[]) => {
+                const previousToken = currentRoute[currentRoute.length - 1] ?? tokenSource;
+                if (compareAddress(previousToken, normalizedTokenDestination)) {
+                    paths.push(currentRoute);
+                    return;
+                }
+
+                if (depth === MAX_DEPTH) {
+                    return;
+                }
+
+                for (let i = 0; i < pools.length; i++) {
+                    const tokenOut = tokenOutWhenSwapPool(pools[i], previousToken);
+                    if (!tokenOut) {
+                        continue;
+                    }
+                    if (pathContainsToken(currentRoute, tokenOut)) {
+                        continue;
+                    }
+                    const poolsCopy = [...availablePools];
+                    poolsCopy.splice(i, 1);
+                    computeRoute([...currentRoute, tokenOut], depth + 1, poolsCopy);
+                }
+            };
+            computeRoute([normalizedTokenSource], 0, pools);
+            return paths;
+        },
+        [tokenOutWhenSwapPool, uniswapConfig.weth],
+    );
+
+    const getBestPathIn = useCallback(
+        async (paths: Address[][], amountIn: string): Promise<Address[]> => {
+            const amountsOut = await Promise.all(
+                paths.map(async (path) => {
+                    try {
+                        const amountOut = await getAmountsOut(amountIn, path);
+                        return amountOut[amountOut.length - 1];
+                    } catch (error) {
+                        return '0';
+                    }
+                }),
+            );
+            const maxAmountOutIndex = amountsOut.indexOf(
+                maxBigInt(...amountsOut.map((amount) => BigInt(amount))).toString(),
+            );
+            return paths[maxAmountOutIndex] ?? [];
+        },
+        [getAmountsOut],
+    );
+
+    const getBestPathWithExactIn = useCallback(
         async (tokenIn: Address, tokenOut: Address, amountIn: string) => {
-
             if (compareAddress(tokenIn, tokenOut)) {
-                console.log("tokenIn and tokenOut are the same");
                 return [];
             }
 
-            if (amountIn === "0") {
-                console.log("amountIn is 0");
+            if (amountIn === '0') {
                 return [];
             }
 
-            const pools = await getPools();
+            const normalizedPools = Object.keys(pools).map((poolAddress) => {
+                const pool = pools[poolAddress];
+                return [pool.token0, pool.token1, poolAddress] as PoolData;
+            });
 
-            
-
-            const allPaths = getAllPaths(tokenIn, tokenOut, pools);
-
-
+            const allPaths = getAllPaths(tokenIn, tokenOut, normalizedPools);
 
             const bestPath = await getBestPathIn(allPaths, amountIn);
 
             return bestPath;
+        },
+        [getAllPaths, getBestPathIn, pools],
+    );
 
-        }
-    , [getAmountsOut, pools]);
-    
+    const getBestPathOut = useCallback(
+        async (paths: Address[][], amountOut: string): Promise<Address[]> => {
+            const amountsIn = await Promise.all(
+                paths.map(async (path) => {
+                    try {
+                        const amountIn = await getAmountsIn(amountOut, path);
+                        return amountIn[0];
+                    } catch (error) {
+                        return undefined;
+                    }
+                }),
+            );
+            const minAmountInIndex = amountsIn.indexOf(
+                minBigInt(
+                    ...amountsIn.filter((value) => value != undefined).map((amount) => BigInt(amount)),
+                ).toString(),
+            );
+            return paths[minAmountInIndex] ?? [];
+        },
+        [getAmountsIn],
+    );
 
     const pathContainsToken = (path: Address[], token: Address): boolean => {
         return path.includes(token);
-    }
-
-    const getAllPaths = (tokenSource: Address, tokenDestination: Address, pools: PoolData[] ) =>  {
-        const paths: Address[][] = [];
-        const MAX_DEPTH = 3;
-
-        const computeRoute  = (currentRoute: Address[], depth: number) => {
-            if (compareAddress(currentRoute[currentRoute.length - 1], tokenDestination)) {
-                paths.push(currentRoute);
-                return;
-            }
-
-            if (depth === MAX_DEPTH) {
-                return;
-            }
-
-            for(let i = 0; i < pools.length; i++) {
-                
-                const tokenOut  = tokenOutWhenSwapPool(pools[i], currentRoute[currentRoute.length - 1]);
-
-                if (!tokenOut) {
-                    continue;
-                }
-
-                if (pathContainsToken(currentRoute, tokenOut)) {
-                    continue;
-                }
-
-
-                computeRoute([...currentRoute, tokenOut], depth + 1);
-            }
-        }
-
-        computeRoute([tokenSource], 0);
-
-        return paths;
-    }
-
-    async function getBestPathIn(paths: Address[][], amountIn: string): Promise<Address[]> {
-        const amountsOut = await Promise.all(paths.map(async (path) => {
-            try {
-                const amountOut =  await getAmountsOut(amountIn, path);
-
-                return amountOut[amountOut.length - 1];
-            } catch (error) {
-                return "0";
-            }
-        }));
-    
-        const maxAmountOutIndex = amountsOut.indexOf(maxBigInt(...amountsOut.map(amount => BigInt(amount))).toString());
-        return paths[maxAmountOutIndex] ?? [];
-    }
+    };
 
     const getBestPathWithExactOut = useCallback(
         async (tokenIn: Address, tokenOut: Address, amountOut: string) => {
-
             if (compareAddress(tokenIn, tokenOut)) {
-                console.log("tokenIn and tokenOut are the same");
                 return [];
             }
-
-            if (amountOut === "0") {
-                console.log("amountOut is 0");
+            if (amountOut === '0') {
                 return [];
             }
-
-            
-
-            const pools = await getPools();
-
-            const allPaths = getAllPaths(tokenIn, tokenOut, pools);
-
+            const normalizedPools = Object.keys(pools).map((poolAddress) => {
+                const pool = pools[poolAddress];
+                return [pool.token0, pool.token1, poolAddress] as PoolData;
+            });
+            const allPaths = getAllPaths(tokenIn, tokenOut, normalizedPools);
             const bestPath = await getBestPathOut(allPaths, amountOut);
-
             return bestPath;
-
-        }
-    , [getAmountsIn, pools]);
-
-    async function getBestPathOut(paths: Address[][], amountOut: string): Promise<Address[]> {
-        const amountsIn = await Promise.all(paths.map(async (path) => {
-            try {
-                const amountIn =  await getAmountsIn(amountOut, path);
-
-                console.log(amountIn);
-                return amountIn[0];
-            } catch (error) {
-                return undefined;
-            }
-        }));
-
-        console.log("amounts in:", amountsIn);
-
-
-
-    
-        const minAmountInIndex = amountsIn.indexOf(minBigInt(...amountsIn.filter((value) => value != undefined).map(amount => BigInt(amount))).toString());
-        // Devuelve el camino con el mínimo valor que no sea 0
-
-        console.log("minAmount", minBigInt(...amountsIn.filter((value) => value != undefined).map(amount => BigInt(amount))).toString());
-        return paths[minAmountInIndex] ?? [];
-    }
+        },
+        [getAllPaths, getBestPathOut, pools],
+    );
 
     const minBigInt = (...values: bigint[]): bigint | undefined => {
-        return values.reduce((min, value) => !min || value < min ? value : min);
-    }
-
-    
-
+        return values.reduce((min, value) => (!min || value < min ? value : min));
+    };
 
     const maxBigInt = (...values: bigint[]): bigint => {
-        return values.reduce((max, value) => value > max ? value : max);
-    }
+        return values.reduce((max, value) => (value > max ? value : max));
+    };
 
     const compareAddress = (a: Address, b: Address): boolean => {
         return a.toLocaleLowerCase() === b.toLocaleLowerCase();
-    }
-    
+    };
 
-    const tokenOutWhenSwapPool = (pool: PoolData, tokenIn: Address): Address | undefined => {
-        if (compareAddress(pool[0], tokenIn)) {
-            return pool[1];
-        }
-        else if (compareAddress(pool[1], tokenIn)) {
-            return pool[0];
-        }
-        else {
-            return undefined;
-        }
-    }
-
-    return { getBestPathWithWithExactIn, getBestPathWithExactOut };
-}
+    return { getBestPathWithWithExactIn: getBestPathWithExactIn, getBestPathWithExactOut };
+};
--- src/Hooks/useRouter.ts
+++ src/Hooks/useRouter.ts
@@ -1,9 +1,9 @@
-import { Address } from "viem";
-import { readContract } from "wagmi/actions";
-import { useUniswap } from "./useUniswap";
+import { Address } from 'viem';
+import { readContract } from 'wagmi/actions';
+import { useUniswap } from './useUniswap';
 import UniswapV2Router02ABI from '../ABI/UniswapV2Router02.json';
-import { useCallback } from "react";
-import { useConfig } from "./useConfig";
+import { useCallback } from 'react';
+import { useConfig } from './useConfig';
 
 export const useRouter = () => {
     const { uniswapConfig } = useUniswap();
@@ -11,48 +11,48 @@ export const useRouter = () => {
 
     const getAmountsOut = useCallback(
         async (amountIn: string, path: Address[]) => {
-            const data: bigint[] = await readContract({
+            const data: bigint[] = (await readContract({
                 address: uniswapConfig.router,
                 abi: UniswapV2Router02ABI,
                 functionName: 'getAmountsOut',
                 args: [amountIn, path],
                 chainId: Number(activeChainConfig.id),
-            }) as bigint[];
+            })) as bigint[];
 
             return data.map((value) => value.toString());
-        }
-    , [uniswapConfig.router]);
+        },
+        [activeChainConfig.id, uniswapConfig.router],
+    );
 
     const getAmountsIn = useCallback(
         async (amountOut: string, path: Address[]) => {
-            const data: bigint[] = await readContract({
+            const data: bigint[] = (await readContract({
                 address: uniswapConfig.router,
                 abi: UniswapV2Router02ABI,
                 functionName: 'getAmountsIn',
                 args: [amountOut, path],
                 chainId: Number(activeChainConfig.id),
-            }) as bigint[];
+            })) as bigint[];
 
             return data.map((value) => value.toString());
-        }
-    , [uniswapConfig.router]);
+        },
+        [uniswapConfig.router],
+    );
 
     const getPair = useCallback(
         async (tokenA: Address, tokenB: Address) => {
-            const data: Address = await readContract({
+            const data: Address = (await readContract({
                 address: uniswapConfig.factory,
                 abi: UniswapV2Router02ABI,
                 functionName: 'getPair',
                 args: [tokenA, tokenB],
                 chainId: Number(activeChainConfig.id),
-            }) as Address;
+            })) as Address;
 
             return data;
-        }
-    , [uniswapConfig.factory]);
-
-    
-    
+        },
+        [uniswapConfig.factory],
+    );
 
     return { getAmountsOut, getAmountsIn, getPair };
-}
+};
