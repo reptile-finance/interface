b3bbd787458d03bbfa4db24cfb459f42ed4af0c5
GabrielMartinezRodriguez <gabrielmartinezrdrgz@gmail.com>
2024-01-03 13:32:56 +0100

    feat: farm add

---
+++ src/Components/EarnAdCard/index.tsx
@@ -0,0 +1,34 @@
+import { useNavigate } from 'react-router-dom';
+import {
+    EarnActionButton,
+    EarnAdActions,
+    EarnAdCardBody,
+    EarnAdCardContent,
+    EarnAdCardImage,
+    EarnAdCardWrapper,
+    EarnAddCardHeader,
+} from './Styles';
+
+export const EarnAdCard = () => {
+    const navigate = useNavigate();
+
+    return (
+        <EarnAdCardWrapper>
+            <EarnAdCardContent>
+                <EarnAddCardHeader>
+                    <EarnAdCardImage />
+                    Improve your earnings
+                </EarnAddCardHeader>
+                <EarnAdCardBody>
+                    Contribute by providing liquidity and maximize your income by participating in staking farms. By
+                    locking your LP Tokens in these farms, you not only support the stability of the ecosystem but also
+                    earn rewards in the form of Reptile Tokens. This strategy allows you to significantly increase your
+                    assets, fully leveraging the potential of your cryptocurrency investments
+                </EarnAdCardBody>
+                <EarnAdActions>
+                    <EarnActionButton onClick={() => navigate('/earn')}> Go To Farms </EarnActionButton>
+                </EarnAdActions>
+            </EarnAdCardContent>
+        </EarnAdCardWrapper>
+    );
+};
