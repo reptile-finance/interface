8fb5983781aada31243b1ea9c7aa737c55e1386d
0xshrimpsoup <xshrimpsoup@proton.me>
2024-03-02 20:38:48 +0100

    fix: Blockchain provider URL

--- src/Providers/Blockchain.ts
+++ src/Providers/Blockchain.ts
@@ -16,7 +16,7 @@ export const opBnbChain: Chain = {
     blockExplorers: {
         default: {
             name: 'opBNBScan',
-            url: 'https://testnet.opbnbscan.com/',
+            url: 'https://testnet.opbnbscan.com',
         },
     },
     rpcUrls: {
